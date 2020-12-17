a738be7f888c203f77f6f65b9a613e58c44e0099
Niko Sward <nicholascsward@gmail.com>
2020-12-16 23:15:07 -0600

    13.1

---
+++ day13/1.js
@@ -0,0 +1,23 @@
+const { time } = require('console');
+const fs = require('fs');
+//const input = fs.readFileSync('test.txt', 'utf-8');
+const input = fs.readFileSync('input.txt', 'utf-8');
+const inputArr = input.split('\n').slice(0, -1);
+const timeStamp = parseInt(inputArr[0], 10);
+const buses = inputArr[1]
+  .split(',')
+  .filter((route) => route !== 'x')
+  .map((route) => parseInt(route, 10));
+
+let resultVal = Infinity;
+let resultBusId;
+
+buses.forEach((bus) => {
+  let next = bus - (timeStamp % bus);
+  if (next < resultVal) {
+    resultVal = next;
+    resultBusId = bus;
+  }
+});
+
+console.log(resultVal * resultBusId);
